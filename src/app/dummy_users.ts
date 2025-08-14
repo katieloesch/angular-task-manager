a19058ec481c361d6262bf3a7b35a0bd533a51bf
Katie Loesch <catherine.loesch@protonmail.com>
2025-08-15 00:15:37 +0100

    update user component with dummy data

---
+++ src/app/dummy_users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Charles Xavier',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Jean Grey',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Erik Lehnsherr',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'James Howlett',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Ororo Munroe',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Remy Lebeau',
+    avatar: 'user-6.jpg',
+  },
+];
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy_users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
-  styleUrl: './user.css'
+  styleUrl: './user.css',
 })
 export class User {
+  selectedUser = DUMMY_USERS[randomIndex];
 
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    console.log('clicked');
+  }
 }
